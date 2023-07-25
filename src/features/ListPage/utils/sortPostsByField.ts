18af882bc722b35cad0eca8637a7f3b1768a876a
Арген Алимбаев <argenalimbaev@MacBook-Air-Argen.local>
2023-07-26 00:16:16 +0600

    make more simplify the sortPostsByField function in features/ListPage/utils

--- src/features/ListPage/utils/sortPostsByField.ts
+++ src/features/ListPage/utils/sortPostsByField.ts
@@ -3,20 +3,19 @@ import { PostsListState } from "../types/types";
 
 export const sortPostsByField = (state: PostsListState, field: string) => {
   state.sortDirection = state.sortDirection === "asc" ? "desc" : "asc";
+
+  const sortMultiplier = state.sortDirection === "asc" ? 1 : -1;
+
   state.posts = quickSortByField(state.posts, (a, b) => {
     if (field === "title") {
-      return (
-        a.title.localeCompare(b.title) *
-        (state.sortDirection === "asc" ? 1 : -1)
-      );
-    } else if (field === "description") {
-      return (
-        a.body.localeCompare(b.body) * (state.sortDirection === "asc" ? 1 : -1)
-      );
-    } else if (field === "id") {
-      return (a.id - b.id) * (state.sortDirection === "asc" ? 1 : -1);
-    } else {
-      return 0;
+      return a.title.localeCompare(b.title) * sortMultiplier;
     }
+    if (field === "description") {
+      return a.body.localeCompare(b.body) * sortMultiplier;
+    }
+    if (field === "id") {
+      return (a.id - b.id) * sortMultiplier;
+    }
+    return 0;
   });
 };
